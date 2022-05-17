1d66ab1def893c3e63cde40c61c6fff683e38929
JoshBenPhilip <joshben4c@gmail.com>
2022-05-17 10:35:21 -0400

    first commit

---
+++ src/components/CCoffee.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+
+
+class CCoffee extends Component {
+    render() {
+        return <h1>Coffee List (CC)</h1>
+    }
+    
+}
+
+export default CCoffee;
